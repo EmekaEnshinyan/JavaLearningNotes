b59cd65fb9be82ebc7a3391a506951bae35f35d6
Emeka Anekwe <59901971+EmekaEnshinyan@users.noreply.github.com>
2021-03-14 16:20:04 -0700

    Create SinoTibetan.java

---
+++ miniproject_languages/SinoTibetan.java
@@ -0,0 +1,15 @@
+public class SinoTibetan extends Language{
+
+public SinoTibetan(String languageName, int speakers){
+  super(languageName, speakers, "Tibeto-Burman", "Subject-Object-Verb");
+  if (this.name.contains("Chinese")){
+     this.wordOrder = "Subject-Verb-Object";
+  }
+}
+@Override
+public void getInfo(){
+  System.out.println(this.name + " is spoken by " + this.numSpeakers + " people mainly in " + this.regionsSpoken + ". The language follows the word order: " + this.wordOrder);
+}
+
+  
+}
