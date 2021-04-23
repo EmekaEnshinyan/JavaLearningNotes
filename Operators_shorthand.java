cb2cf9ddc7bd06c4e4d4dde3b93cebd68892f566
Emeka Anekwe <59901971+EmekaEnshinyan@users.noreply.github.com>
2021-04-23 14:50:06 -0700

    Update and rename Operators & Shorthand to Operators_shorthand.java

---
+++ Operators_shorthand.java
@@ -0,0 +1,39 @@
+/*Some operators are used for short hand:
+
+ex int x = 3 > 1 ? 5 : 10;
+
+this is symantically equivalent to
+
+int x;
+if (3 > 1){
+  x = 5;
+}else{
+  x = 10;
+} */
+example: 
+package com.codegym.task.task02.task0217;
+
+/* 
+Minimum of four numbers
+
+*/
+
+public class Solution {
+    public static int min(int a, int b, int c, int d) {
+        if (min(a, b) < c) {
+            return (min(a, b) <= d) ? min(a, b) : d;
+        } else {
+            return (c <= d) ? c : d;
+        }
+    }
+    public static int min(int a, int b) {
+        return (a <= b) ? a : b;
+
+    }
+    public static void main(String[] args) throws Exception {
+        System.out.println(min(-20, -10));
+        System.out.println(min(-20, -10, -30, -40));
+        System.out.println(min(-20, -10, -30, 40));
+        System.out.println(min(-40, -10, -30, 40));
+    }
+}
